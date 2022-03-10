f7ab365ca18d2009b90d57882781a9e79fb949fb
root <chelsea.lutta@gmail.com>
2022-03-10 13:31:04 -0800

    4 puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,27 @@
 #include <stdio.h>
+
 #include <string.h>
+
+
+
 /**
- *  Function main 
- */
+ *
+ *  * main - puts
+ *
+ *   * @nb: The number to be checked
+ *
+ *    *
+ *
+ *     * Return: 0
+ *
+ *      */
+
 int main(void)
+
 {
+
 puts("\"Programming is like building a multilingual puzzle");
+
 return (0);
+
 }
