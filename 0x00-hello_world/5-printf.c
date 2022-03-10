b10890702bf2feee07381934fae06432f6ff2e3e
root <chelsea.lutta@gmail.com>
2022-03-10 13:44:33 -0800

    Printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+* main - its main
+* some statement
+* Return: 0 otherwise
+*/
+int main(void)
+{
+printf("with proper grammer, but the outcome is a piece of art,");
+return (0);
+}
