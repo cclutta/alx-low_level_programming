c597dcddefdde8ac31bf90141bd40ba0a8b81080
root <chelsea.lutta@gmail.com>
2022-03-11 11:30:02 -0800

    Size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,17 +7,18 @@
  *
  * Return: 0
  */
-int main(void){
-	char c;
-	int i;
-	long int li;
-	long long int lli;
-	float f;
+int main(void)
+{
+char c;
+int i;
+long int li;
+long long int lli;
+float f;
 
 printf("Size of a char: %zu byte(s)\n", sizeof(c));
 printf("Size of an int: %zu byte(s)\n", sizeof(i));
 printf("Size of a long int: %zu byte(s)\n", sizeof(li));
 printf("Size of a long long int: %zu byte(s)\n", sizeof(lli));	
-printf("Size of a float: %zu byte(s)\n",sizeof(f));
+printf("Size of a float: %zu byte(s)\n", sizeof(f));
 return (0);
 }
