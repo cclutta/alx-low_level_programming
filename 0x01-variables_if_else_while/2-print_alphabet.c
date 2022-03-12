4369ca3d615b3c179b1fc66d3772249dab2bc037
root <chelsea.lutta@gmail.com>
2022-03-11 20:34:08 -0800

    Print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - its main
+ *
+ * Return: 0
+ */
+int main(void)
+{
+for (int x = 'A'; x <='Z'; x++)
+{
+putchar(tolower(x));
+}
+return (0);
+}
