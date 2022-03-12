6ce93cf76c7bd6ecd41a2faa97377b992b672933
root <chelsea.lutta@gmail.com>
2022-03-12 11:08:35 -0800

    Print numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * main - its main
+ *
+ * Return: 0
+ */
+int main(void)
+{
+int x;
+for (x = 0; x < 10; x++)
+
+	putchar(x);
+
+putchar('\n');
+return (0);
+}
