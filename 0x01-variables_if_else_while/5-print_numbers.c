0121f72d9b2624707a89d35f410585757d4fa5b3
root <chelsea.lutta@gmail.com>
2022-03-12 11:04:38 -0800

    Print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * main - its main
+ *
+ * Return: 0
+ */
+int main(void)
+{
+int x;
+for (x = 0; x < 10; x++){
+	printf("%d", x);
+}
+putchar('\n');
+return (0);
+}
