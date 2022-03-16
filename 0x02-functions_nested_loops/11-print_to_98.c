d00b0dbf425dc23a29c5d676d7ed6c301036adec
cclutta <52520502+cclutta@users.noreply.github.com>
2022-03-16 22:24:56 +0300

    Update 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -32,6 +32,24 @@ putchar(' ');
 }
 
 
-
 return (0);
 }
+
+void print_num(int n)
+{
+if (n < 0) {
+putchar('-');
+n = -n;
+}
+if ( n / 10 )
+print_num( n / 10);
+ 
+putchar( n%10 + '0');
+}
+
+
+
+
+
+
+
