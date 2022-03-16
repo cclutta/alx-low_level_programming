8dbd9e4d043b6c69e7a07d2663abbd5c22086249
cclutta <52520502+cclutta@users.noreply.github.com>
2022-03-16 18:43:13 +0300

    Update 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -13,14 +13,8 @@
 int print_last_digit(int n)
 {
 int r;
-r = (abs(n)) % 10;
-print_num(r);
+r = n % 10;
+_putchar(r);
 return (r);
 
-}
-void print_num(int n){
-    if (n/10)
-        print_num(n/10);
- 
-    putchar(n%10 + '0');
 }
