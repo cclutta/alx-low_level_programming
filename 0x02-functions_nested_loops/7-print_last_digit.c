0c5fca8ab19c9c050679e95988451aeb1671ca41
cclutta <52520502+cclutta@users.noreply.github.com>
2022-03-16 17:44:17 +0300

    Create 7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+
+
+/**
+ * print_last_digit- its main
+ * @n: bitch
+ *
+ * Return: 0
+ */
+int print_last_digit(int n)
+{
+
+return (n%10);
+
+}
