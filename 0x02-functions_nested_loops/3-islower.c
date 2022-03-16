21a9b9cf927101a6f12765213d3731978758d534
cclutta <52520502+cclutta@users.noreply.github.com>
2022-03-16 14:00:46 +0300

    Create 3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+
+
+/**
+ * _islower - its main
+ *
+ * Return: 0
+ */
+int _islower(int c)
+{
+if (c >= 'a' && c <= 'z')
+{
+return (1);
+}
+else 
+{
+  return (0);
+}
+
+
+}
+
+
