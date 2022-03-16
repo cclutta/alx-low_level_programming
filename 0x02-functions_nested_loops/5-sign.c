25e7abf90755d00a6e2ed04b709741f72bc9ec48
cclutta <52520502+cclutta@users.noreply.github.com>
2022-03-16 14:20:24 +0300

    Create 5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+
+
+/**
+ * print_sign- its main
+ * @n: bitch
+ *
+ * Return: 0
+ */
+int print_sign(int n)
+{
+if (n > 0)
+{
+putchar('+');
+return (1);
+}
+else if (n == 0)
+{
+putchar('0');
+return (0);
+}
+else
+{
+putchar('-');
+return (-1);
+}
+
+}
