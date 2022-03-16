dfff1f64ce1363d1a6b658852c46e6d1d55a3f74
cclutta <52520502+cclutta@users.noreply.github.com>
2022-03-16 13:41:39 +0300

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,1 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
 
+
+/**
+ * main - its main
+ *
+ * Return: 0
+ */
+int main(void)
+{
+
+char i;
+
+for (i = 'A'; i <= 'Z'; i++)
+{
+putchar(tolower(i));
+}
+putchar('\n');
+return (0);
+}
