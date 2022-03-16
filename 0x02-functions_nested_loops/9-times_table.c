89f5499d33ab7a3f94b4fe90d781f0b1daf73c18
cclutta <52520502+cclutta@users.noreply.github.com>
2022-03-17 02:25:44 +0300

    Create 9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,47 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+
+
+
+/**
+ * times_table - its main
+ * @n: bitch
+ *
+ * Return: 0
+ */
+void times_table(void)
+{
+int i, j;
+for (i = 0; i <= 9; i++)
+{
+for (j = 0; j <= 9; j++)
+{
+print_num(i * j);
+if (i != 9){
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+}
+
+
+}
+/**
+ * print_num - its main
+ * @n: bitch
+ *
+ * Return: 0
+ */
+void print_num(int n)
+{
+if (n < 0)
+{
+putchar('-');
+n = -n;
+}
+if (n / 10)
+print_num(n / 10);
+putchar(n % 10 + '0');
+}
