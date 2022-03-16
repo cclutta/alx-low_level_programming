f8f5b21a676296e287e679c4457bce2766d526c7
cclutta <52520502+cclutta@users.noreply.github.com>
2022-03-16 14:12:38 +0300

    Create 4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+
+
+/**
+ * _isalpha- its main
+ * @c: bitch
+ *
+ * Return: 0
+ */
+int _islower(int c)
+{
+if ((c >= 'a' && c <= 'z') && (c >= 'A' && c <= 'Z'))
+{
+return (1);
+}
+else
+{
+return (0);
+}
+
+
+}
