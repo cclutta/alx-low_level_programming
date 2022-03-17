df35fc7ea4bf4b1aeded6ae6583cc9f683e90cf1
cclutta <52520502+cclutta@users.noreply.github.com>
2022-03-17 23:49:48 +0300

    Update 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -6,7 +6,7 @@
  * Return: Always 0.
  */
 int main(void)
-
+{
 	int i;
 	
 	for (i = 1; i <= 100; i++)
@@ -16,7 +16,7 @@ int main(void)
 		}
 		 else if (i % 5 == 0){
 			printf("Buzz ");
-			if(i%3== 0){
+			if(i % 3 == 0){
 			    printf("FizzBuzz ");
 			}
 		 }
