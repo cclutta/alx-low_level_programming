a61da38daa73a85126e0555725233097f7b1df5e
cclutta <52520502+cclutta@users.noreply.github.com>
2022-03-17 12:22:33 +0300

    Update 8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -8,12 +8,12 @@
  * Return: Always (0)
  */
 
-void print_square(int size)
+void print_square(int n)
 {
 	int i, j;
-	for (i = 0; i < size; i++)
+	for (i = 0; i < ; i++)
 	{
-		for(j = 0; j < size; j++)
+		for(j = 0; j < n; j++)
 		{
 			_putchar('#');
 		}
