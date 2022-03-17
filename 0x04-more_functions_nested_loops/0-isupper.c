4a4a323d0274e94b858eeac26cd16a23cf700137
cclutta <52520502+cclutta@users.noreply.github.com>
2022-03-17 16:07:16 +0300

    Create 0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _isupper - print square
+ * @c: input size
+ *
+ * Description: prints a square
+ * Return: Always (0)
+ */
+
+int _isupper(int c)
+{
+  if (isupper(c) == 0)
+  {
+    return (0);
+  }
+  else
+  {
+    return (1);
+  }
+}
+
