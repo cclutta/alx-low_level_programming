9592dcf3f0e66b498f423f4673a51ef4093029ec
cclutta <52520502+cclutta@users.noreply.github.com>
2022-03-17 19:53:22 +0300

    Create 2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - multiply
+ * @a: size
+ * @b: size
+ *
+ * Description: prints a square
+ * Return: Always (0)
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
