2501782a8b0d98acdbbb8fbb8d5abfcc2d66ba41
cclutta <52520502+cclutta@users.noreply.github.com>
2022-03-18 13:54:18 +0300

    Update 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,16 +9,16 @@
 
 void more_numbers(void)
 {
-  int i;
-  int j;
-  
-  for (i = 0; i <= 9; i++)
-  {
-    for (j = 0; j <= 14; j++)
-    {
-      _putchar(j + '0');
-    }
-    _putchar('\n');
-  }
-  _putchar('\n');
+	int i;
+	int j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			_putchar(j + '0');
+		}
+		_putchar('\n');
+	}
+	_putchar('\n');
 }
