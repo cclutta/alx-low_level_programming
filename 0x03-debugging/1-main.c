5b793d27ed35743dee6a923f6b07fdf315e27de9
cclutta <52520502+cclutta@users.noreply.github.com>
2022-03-18 17:25:12 +0300

    Update 1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -9,6 +9,7 @@
 int main(void)
 {
 int i;
+
 printf("Infinite loop incoming :(\n");
 i = 0;
 
@@ -17,7 +18,6 @@ i = 0;
 /* { */
 /* putchar(i); */
 /* }*/
-
 
 printf("Infinite loop avoided! \\o/\n");
 return (0);
