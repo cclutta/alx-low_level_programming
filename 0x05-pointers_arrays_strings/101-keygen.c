2c4fa35d65e35b55a26e1c250c2c581ec8c5c8b2
cclutta <52520502+cclutta@users.noreply.github.com>
2022-03-22 01:04:20 +0300

    Create 101-keygen.c

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,51 @@
+#include "main.h"
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int i = 0;
+	int randomizer = 0;
+
+	srand((unsigned int)(time(NULL)));
+
+	char numbers[] = "0123456789";
+	char up_letters[] = "ABCDEFGHIJKLMNOQPRSTUYWVZX";
+	char low_letters[] = "abcdefghijklmnoqprstuvwyzx";
+	char symbols[] = "!@#$^&*?";
+
+	char pass[20];
+
+	randomizer = rand() % 4;
+
+	for (i = 0; i < 20; i++)
+	{
+		if (randomizer == 1) {
+            pass[i] = numbers[rand() % 10];
+            randomizer = rand() % 4;
+            printf("%c", pass[i]);
+        }
+        else if (randomizer == 2) {
+            pass[i] = symbols[rand() % 8];
+            randomizer = rand() % 4;
+            printf("%c", pass[i]);
+        }
+        else if (randomizer == 3) {
+            pass[i] = up_letters[rand() % 26];
+            randomizer = rand() % 4;
+            printf("%c", pass[i]);
+        }
+        else {
+            pass[i] = low_letters[rand() % 26];
+            randomizer = rand() % 4;
+            printf("%c", pass[i]);
+        }
+	}
+
+}
