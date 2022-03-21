062d5f3c1080a8e6012bc9472686463195d45d73
cclutta <52520502+cclutta@users.noreply.github.com>
2022-03-21 14:54:42 +0300

    Update 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,20 +9,20 @@
  */
 void print_rev(char *s)
 {
-  int i;
-  
+	int i = 0;
+
 	while (*s != '\0')
 	{
 		s++;
 		i++;
 	}
-	
+
 	while (i > 0)
 	{
 		s--;
 		_putchar(*s);
 		i--;
 	}
-	
+
 	_putchar('\n');
 }
