bb3735ea59bad4bca898135a694c71ea52521d3a
cclutta <52520502+cclutta@users.noreply.github.com>
2022-03-21 14:23:00 +0300

    Create 2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string.
+ * @s: pointer
+ * Description: returns string
+ *
+ * Return: int.
+ */
+int _strlen(char *s)
+{
+	return (sizeof(s));
+}
