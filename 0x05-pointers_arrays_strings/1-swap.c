bf4f5742892cd13f52881504cda96f66651c8e9a
cclutta <52520502+cclutta@users.noreply.github.com>
2022-03-21 14:08:19 +0300

    Update 1-swap.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -3,7 +3,7 @@
 /**
  * swap_int - meter and updates the value it points to to 98.
  * @a: pointer
- * @b - pointer
+ * @b: pointer
  * Description: swaps numbers
  *
  * Return: Void.
@@ -11,10 +11,10 @@
 void swap_int(int *a, int *b)
 {
 	int x;
-	
+
 	x = *a;
-	
+
 	*a = *b;
-	
+
 	*b = x;
 }
