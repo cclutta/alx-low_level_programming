c5c2f4a51b2ebb1b45a78282c2d71acf0933947e
cclutta <52520502+cclutta@users.noreply.github.com>
2022-03-21 18:34:28 +0300

    Create 5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * rev_string - unction that reverses a string.
+ * @s: pointer
+ * Description: returns string
+ *
+ * Return: Void.
+ */
+void rev_string(char *s);
+{
+	int i = 0;
+	char *_s = s;
+	char temp[2000];
+	
+	while (*s != '\0')
+	{
+		temp[i] = *s;
+		s++;
+		i++;
+	}
+
+	i = 0;
+
+	while (s > _s)
+	{
+		s--;
+		*s = temp[i];
+		i++;
+	}
+}
+	
