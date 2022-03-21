a3c45ddf8fb37bdaaf58222840aef58cdea99026
cclutta <52520502+cclutta@users.noreply.github.com>
2022-03-22 01:06:11 +0300

    Update 101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -12,15 +12,13 @@ int main(void)
 {
 	int i = 0;
 	int randomizer = 0;
-
-	srand((unsigned int)(time(NULL)));
-
 	char numbers[] = "0123456789";
 	char up_letters[] = "ABCDEFGHIJKLMNOQPRSTUYWVZX";
 	char low_letters[] = "abcdefghijklmnoqprstuvwyzx";
 	char symbols[] = "!@#$^&*?";
-
 	char pass[20];
+	
+	srand((unsigned int)(time(NULL)));
 
 	randomizer = rand() % 4;
 
