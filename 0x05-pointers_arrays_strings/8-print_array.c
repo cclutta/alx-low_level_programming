3ee89feef99d0c8d4e0f2b19f37220c68c4a5c4a
cclutta <52520502+cclutta@users.noreply.github.com>
2022-03-22 00:16:25 +0300

    Create 8-print_array.c

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_array - that prints half of a string, followed by a new line.
+ * @a: pointer
+ * @n: size
+ * Description: returns string
+ *
+ * Return: Void.
+ */
+void print_array(int *a, int n)
+{
+	int i = 0;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+
+		if (i != n-1)
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
+}
