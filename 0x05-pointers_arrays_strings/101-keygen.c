29381cf89d1b398ec0d4f66902f768046142d5b9
cclutta <52520502+cclutta@users.noreply.github.com>
2022-03-22 01:09:35 +0300

    Update 101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -10,41 +10,26 @@
 
 int main(void)
 {
-	int i = 0;
-	int randomizer = 0;
-	char numbers[] = "0123456789";
-	char up_letters[] = "ABCDEFGHIJKLMNOQPRSTUYWVZX";
-	char low_letters[] = "abcdefghijklmnoqprstuvwyzx";
-	char symbols[] = "!@#$^&*?";
-	char pass[20];
-	
-	srand((unsigned int)(time(NULL)));
+	int i, sum, n;
+	int pass[100];
 
-	randomizer = rand() % 4;
+	sum = 0;
 
-	for (i = 0; i < 20; i++)
+	srand(time(NULL));
+
+	for (i = 0; i < 100; i++)
 	{
-		if (randomizer == 1) {
-            pass[i] = numbers[rand() % 10];
-            randomizer = rand() % 4;
-            printf("%c", pass[i]);
-        }
-        else if (randomizer == 2) {
-            pass[i] = symbols[rand() % 8];
-            randomizer = rand() % 4;
-            printf("%c", pass[i]);
-        }
-        else if (randomizer == 3) {
-            pass[i] = up_letters[rand() % 26];
-            randomizer = rand() % 4;
-            printf("%c", pass[i]);
-        }
-        else {
-            pass[i] = low_letters[rand() % 26];
-            randomizer = rand() % 4;
-            printf("%c", pass[i]);
-        }
-	
-}
+		pass[i] = rand() % 78;
+		sum += (pass[i] + '0');
+		putchar(pass[i] + '0');
+		if ((2772 - sum) - '0' < 78)
+		{
+			n = 2772 - sum - '0';
+			sum += n;
+			putchar(n + '0');
+			break;
+		}
+	}
+
 	return (0);
 }
