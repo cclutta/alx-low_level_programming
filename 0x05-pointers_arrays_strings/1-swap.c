ae015a620eddf22677609e39dfa0d3dda893480d
cclutta <52520502+cclutta@users.noreply.github.com>
2022-03-21 14:06:16 +0300

    Create 1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * swap_int - meter and updates the value it points to to 98.
+ * @a: pointer
+ * @b - pointer
+ * Description: swaps numbers
+ *
+ * Return: Void.
+ */
+void swap_int(int *a, int *b)
+{
+	int x;
+	
+	x = *a;
+	
+	*a = *b;
+	
+	*b = x;
+}
