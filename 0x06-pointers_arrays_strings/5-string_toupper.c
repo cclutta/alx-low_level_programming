dde60b9f83b8abf56b2d5cbd0504fd6fe0ba3b31
cclutta <52520502+cclutta@users.noreply.github.com>
2022-03-23 20:31:10 +0300

    Create 5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+/**
+ * string_toupper -  that changes all lowercase letters of a string to uppercase.
+ * @str: param pointer to a char
+ *
+ * Return: char
+ */
+
+char *string_toupper(char *str)
+{
+	int i; 
+	
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] -= 32;
+		}
+		
+	}
+	return (str);
+
+}
