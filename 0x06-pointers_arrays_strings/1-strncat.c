63798c7f8a31c6927dda3480ae51beb06cc8b2f4
cclutta <52520502+cclutta@users.noreply.github.com>
2022-03-23 16:09:59 +0300

    Update 1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,24 +1,32 @@
 /**
- * *_strcat -  that concatenates two strings.
+ * *_strncat -  that concatenates two strings.
  * @dest: param pointer to a char
  * @src: param pointer to a char
+ * @n: number
  *
  * Return: pointer
  */
 
-char *_strcat(char *dest, char *src)
+char *_strncat(char *dest, char *src, int n)
 {
 	int i;
 	int j;
 
 	i = 0;
 	j = 0;
+	
 
 	while (dest[i] != '\0')
 	{
 		i++;
 	}
+	
 	while (src[j] != '\0')
+	{
+		j++;
+	}
+	
+	if (*src != '\0' && j < n)
 	{
 		dest[i] = src[j];
 		i++;
