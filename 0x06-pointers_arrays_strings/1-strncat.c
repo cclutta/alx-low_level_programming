115dd6fde53b63922255bbea0853654cafca38c4
cclutta <52520502+cclutta@users.noreply.github.com>
2022-03-23 16:23:00 +0300

    Update 1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -11,15 +11,15 @@ char *_strncat(char *dest, char *src, int n)
 {
 	int i;
 	int j;
-	
-	
+
+
 	i = 0;
 
 	while (dest[i] != '\0')
 	{
 		i++;
 	}
-	
+
 	for (j = 0; src[j] != '\0' && j < n; j++)
 	{
 		dest[j + i] = src[j];
