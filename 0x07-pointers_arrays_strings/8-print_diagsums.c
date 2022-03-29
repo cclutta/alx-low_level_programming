d3ba3c421b8c704c468c3edc682ae92f7aba0955
cclutta <52520502+cclutta@users.noreply.github.com>
2022-03-29 20:55:49 +0300

    Create 8-print_diagsums.c

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,10 @@
+#include "main.h"                                                                
+                                                                                 
+/**                                                                              
+ * print_chessboard -  that prints the chessboard.                               
+ * @a: pointer to char                                                           
+ *                                                                               
+ * Return: void                                                                  
+ */                                                                              
+                                                                                 
+void print_diagsums(int *a, int size)
