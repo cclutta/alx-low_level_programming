a712f1936fad7eba64b88d672d1c0cee1be1394d
cclutta <52520502+cclutta@users.noreply.github.com>
2022-03-29 21:00:35 +0300

    Update 8-print_diagsums.c

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,10 +1,24 @@
-#include "main.h"                                                                
-                                                                                 
-/**                                                                              
- * print_chessboard -  that prints the chessboard.                               
- * @a: pointer to char                                                           
- *                                                                               
- * Return: void                                                                  
- */                                                                              
-                                                                                 
+#include "main.h"
+
+/**             
+ * print_diagsums -  that prints the chessboard.
+ * @a: pointer to char
+ * @size: the size
+ *
+ * Return: void
+ */
+
 void print_diagsums(int *a, int size)
+{
+	int i;
+	int diag1 = 0;
+	int diag2 = 0;
+	
+	for (i = 0; i < size; i++)
+	{
+		diag1 += *(a + (size * i + i));
+		diag2 += *(a + (size * i + size - 1 - i));
+	}
+	printf("%d, %d\n", diag1, diag2);
+}
+
