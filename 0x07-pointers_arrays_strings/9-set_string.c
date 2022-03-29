cfe9034781200ab922704b51d03ae4328dcba713
cclutta <52520502+cclutta@users.noreply.github.com>
2022-03-29 21:05:04 +0300

    Create 9-set_string.c

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * set_string -  that prints the chessboard.
+ * @s: pointer to char
+ * @to: pointer
+ *
+ * Return: void
+ */
+
+void set_string(char **s, char *to)
+{
+*s = to;
+}
