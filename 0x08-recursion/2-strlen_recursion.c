6a5cde2dc78ad8d852efaf0b453f91a210da4bba
cclutta <52520502+cclutta@users.noreply.github.com>
2022-03-30 17:46:54 +0300

    Update 2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -7,13 +7,14 @@
 * Return: int
 */
 
-int _strlen_recursion(char *s){
-    if (!*s)
-    {
-        return (0);
-    }
-    else
-    {
-        return (1 + _strlen_recursion(s + 1));
-    }
+int _strlen_recursion(char *s)
+{
+	if (!*s)
+	{
+		return (0);
+	}
+	else
+	{
+		return (1 + _strlen_recursion(s + 1));
+	}
 }
