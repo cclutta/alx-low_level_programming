38769d35d3a3b42c9ad166d5bedce52440a59725
cclutta <52520502+cclutta@users.noreply.github.com>
2022-03-30 17:42:06 +0300

    Create 2-strlen_recursion.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+/**
+* _strlen_recursion - that returns the length of a string.
+* @s: pointer
+*
+* Return: int
+
+int _strlen_recursion(char *s){
+    if (!*s)
+	{
+        return (0);
+    }
+    else
+    {
+        return (1 + _strlen_recursion(s + 1));
+    }
+}
