5347bb9d1fc0587f1b5821b008c9ed700024e9a9
cclutta <52520502+cclutta@users.noreply.github.com>
2022-03-30 12:02:37 +0300

    Create 1-print_rev_recursion.c

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+ #include "main.h"
+/**
+ * _print_rev_recursion - that prints a string, followed by a new line.
+ * @s: pointer
+ *
+ * Return: Void
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+	    _print_rev_recursion(s + 1);
+	    putchar(*s);
+	}
+}
