8990a69d7885dcc38f02bf3d54e56fcd2e07f208
cclutta <52520502+cclutta@users.noreply.github.com>
2022-04-01 22:34:50 +0300

    Create 3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* main - check
+* @argc: count
+* @argv: char pointer
+*
+* Return: int
+*/
+
+int main(int argc, char *argv[])
+{
+	if (argc < 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	
+	printf("%d\n", argv[1] * argv[2]);
+	return (0);
+}
