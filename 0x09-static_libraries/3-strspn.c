06f23face7a9edd1dddc7353ef1265e4d5cdb55d
cclutta <52520502+cclutta@users.noreply.github.com>
2022-04-01 23:24:25 +0300

    Create 3-strspn.c

---
+++ 0x09-static_libraries/3-strspn.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+* _strspn -  that gets the length of a prefix substring.
+* @s: pointer
+* @accept: pointer
+*
+* Return: unsigned int
+*/
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i = 0;
+	char *a = accept;
+
+	while (*s++)
+	{
+		while (*accept++)
+			if (*(s - 1) == *(accept - 1))
+			{
+				i++;
+				break;
+			}
+		if (!(*--accept))
+			break;
+		accept = a;
+	}
+	return (i);
+}
