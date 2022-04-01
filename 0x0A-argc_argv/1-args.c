275f0270be4ea700ec16e892741450769c7c7978
cclutta <52520502+cclutta@users.noreply.github.com>
2022-04-01 21:32:38 +0300

    Create 1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* main - check
+* @argc: count
+* @argv: char pointer
+*
+* Return: int
+*/
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
