8860cc17a08bd147fad50ff4e4d6a910a27c88d7
cclutta <52520502+cclutta@users.noreply.github.com>
2022-04-01 23:18:53 +0300

    Create 9-strcpy.c

---
+++ 0x09-static_libraries/9-strcpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strcpy - t copies the string pointed to by src
+ * @dest: pointer
+ * @src: size
+ * Description: returns string
+ *
+ * Return: char
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+	dest[i] = src[i];
+	}
+
+	dest[i] = '\0';
+
+	return (dest);
+
+}
