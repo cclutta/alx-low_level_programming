4d2e0da0cfdc051548122669a719fe9dbdb15db6
cclutta <52520502+cclutta@users.noreply.github.com>
2022-04-01 23:14:20 +0300

    Create 3-islower.c

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+
+
+/**
+ * _islower - its main
+ * @c: bitch
+ *
+ * Return: 0
+ */
+int _islower(int c)
+{
+if (c >= 'a' && c <= 'z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+
+
+}
