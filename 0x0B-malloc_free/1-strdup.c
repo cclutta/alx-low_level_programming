6de7bfb7d96688c8b298f2b21995306ed88c08fd
cclutta <52520502+cclutta@users.noreply.github.com>
2022-04-05 12:59:07 +0300

    Create 1-strdup.c

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+* _strdup - that creates an array of chars and initializes it
+* @str : char pointer
+*
+* Return: char pointer
+*/
+
+char *_strdup(char *str)
+{
+	char *_str;
+	int i = 0;
+	int j;
+	
+	while (*str != '\0')
+	{
+		i++;
+		str++;
+	}
+	
+	_str = malloc(i * sizeof(char));
+	
+	if (_str == NULL)
+	{
+		return (NULL);
+	}
+	
+	for(j = 0; j < i; j++)
+	{
+		_str[j] = str[j];
+	}
+	
+	return (_str);
+	
+
+
+}
