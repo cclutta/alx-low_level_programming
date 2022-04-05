a549e79c90e8d5b2099023600d6754d089a15412
cclutta <52520502+cclutta@users.noreply.github.com>
2022-04-05 13:20:24 +0300

    Update 1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -12,30 +12,28 @@ char *_strdup(char *str)
 	char *_str;
 	int i = 0;
 	int j;
-	
+
 	if(!str)
 		return (NULL);
-	
+
 	while (*(str + i))
 		i++;
-	
+
 	i++;
-	
-	
+
 	_str = malloc(i * sizeof(char));
-	
+
 	if (_str == NULL)
 	{
 		return (NULL);
 	}
-	
+
 	for(j = 0; j <= i; j++)
 	{
 		_str[j] = str[j];
 	}
-	
+
 	return (_str);
-	
 
 
 }
