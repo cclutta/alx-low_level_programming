9bc6c7bfbf0ea5dc35393d65bd865a3ea4736560
cclutta <52520502+cclutta@users.noreply.github.com>
2022-04-07 23:45:43 +0300

    Update 101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -4,7 +4,7 @@
 * main - check the code
 * @argc: number
 * @argv: array
-* 
+*
 * Return: int
 */
 
@@ -12,28 +12,28 @@ int main(int argc, char *argv[])
 {
 	unsigned int mul;
 	int i, j;
-	
+
 	if (argc != 2)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	
+
 	for (i = 1; i < argc; i++)
 	{
 		for (j = 0; argv[i][j] != '\0'; j++)
 		{
 			if (argv[i][j] < 48 || argv[i][j] > 57)
-			{  
+			{
 				printf("Error\n");
-				exit(98); 
+				exit(98);
 			}
 		}
-		
+
 	}
-	
+
 	mul = atoi(argv[1]) * atoi(argv[2]);
 	printf("%d\n", mul);
-	
+
 	return (0);
 }
