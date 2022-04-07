c775c2bcaae7ffd9cd7a35180c00f6aa92cad8f5
cclutta <52520502+cclutta@users.noreply.github.com>
2022-04-07 17:50:37 +0300

    Create 0-malloc_checked.c

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+/**
+* malloc_checked - that allocates memory using malloc
+* @b: int
+*
+* Return: void
+*/
+
+void *malloc_checked(unsigned int b)
+{
+	char *p;
+	
+	p = malloc(b);
+	
+	if (p == NULL)
+		exit(98);
+	
+	return (p);
+}
