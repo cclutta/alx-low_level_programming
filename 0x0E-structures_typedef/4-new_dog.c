ffc95c7750745cd583bbd1fc4f69ff17b2402d09
cclutta <52520502+cclutta@users.noreply.github.com>
2022-04-11 20:04:45 +0300

    Update 4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -11,4 +11,13 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
+	dog_t *dogt;
+	
+	dogt = malloc(sizeof(dog));
+	
+	dogt->name = name;
+	dogt->age = age;
+	dogt->owner = owner;
+	
+	return (dogt);
 }
