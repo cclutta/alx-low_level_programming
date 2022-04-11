7aad6aa54eb18209e12c4c2a9035f310f3c5ec61
cclutta <52520502+cclutta@users.noreply.github.com>
2022-04-11 16:53:43 +0300

    Update dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,9 +1,16 @@
 #ifndef FILE_DOG
 #define FILE_DOG
+/**
+ * struct dog - dog
+ * @name: char name
+ * @age: float age
+ * @owner: char owner
+ */
+
 
 struct dog
 {
-	char * name;
+	char *name;
 	float age;
 	char *owner;
 };
