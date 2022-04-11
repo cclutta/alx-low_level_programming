6b0d48818b82767987eaf6aee70b3c7e0d054e30
cclutta <52520502+cclutta@users.noreply.github.com>
2022-04-11 17:17:18 +0300

    Create 4-new_dog.c

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,14 @@
+#include "dog.h"
+
+/**
+* new_dog - creates a new dog
+* @name: char
+* @age: float
+* @owner: char
+*
+* Return: void
+*/
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+}
