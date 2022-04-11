63e94c2a767e3dc2093ce02a904e857984edc481
cclutta <52520502+cclutta@users.noreply.github.com>
2022-04-11 20:55:45 +0300

    Update 4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -8,19 +8,16 @@
 *
 * Return: void
 */
-
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dogt;
 	int i = 0, j = 0, k;
 	
 	dogt = malloc(sizeof(dog_t));
-	
 	if (dogt == NULL)
 	{
 		return (NULL);
 	}
-	
 	while (name[i] != '\0')
 	{
 		i++;
@@ -29,33 +26,26 @@ dog_t *new_dog(char *name, float age, char *owner)
 	 {
 		 j++;
 	 }
-	
 	dogt->name = malloc(i * sizeof(dogt->name));
 	if (dogt->name == NULL)
 	{
 		free(dogt->name);
 		return (NULL);
 	}
-	
 	for (k = 0; k <= i; k++)
 	{
 		dogt->name[k] = name[k];
-}
-	
+	}
 	dogt->age = age;
-	
 	dogt->owner = malloc(j * sizeof(dogt->owner));
 	if (dogt->owner == NULL)
 	{
 		free(dogt->owner);
 		return (NULL);
 	}
-	
 	for (k = 0; k <= j; k++)
 	{
 		dogt->owner[k] = owner[k];
-}
-	
-	
+	}
 	return (dogt);
 }
