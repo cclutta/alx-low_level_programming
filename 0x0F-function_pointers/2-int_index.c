6d1d8e98085a4f8a69c73093f1b88082e9440680
cclutta <52520502+cclutta@users.noreply.github.com>
2022-04-14 01:15:21 +0300

    Update 2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,1 +1,34 @@
+#include "function_pointers.h"
 
+/**
+* int_index - that prints a name
+* @array: int pointer
+* @size: size
+* @cmp: function pointer
+*
+* Return: void
+*/
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+	
+	if (array && cmp)
+	{
+		if (size <= 0)
+		{
+			return (-1);
+		}
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i]) != 0)
+			{
+				return (i);
+			}
+			else 
+			{
+				return (-1);
+			}
+		}
+	}
+}
