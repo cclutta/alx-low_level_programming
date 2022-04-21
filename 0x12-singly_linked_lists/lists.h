ceb41c9314421b0f64fb50a917ea60d6fa38137f
cclutta <52520502+cclutta@users.noreply.github.com>
2022-04-21 11:39:18 +0300

    Update lists.h

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,6 +1,10 @@
 #ifndef FILE_LISTS
 #define FILE_LISTS
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
