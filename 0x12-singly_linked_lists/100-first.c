1a2bb1a03c7e87a35a60a0a4ccc43a212eae66bd
cclutta <52520502+cclutta@users.noreply.github.com>
2022-04-21 17:22:54 +0300

    Create 100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+
+void print_first (void) __attribute__((constructor));
+
+/**
+* print_first - prints before main
+*
+* Return: void
+*/
+
+void print_first(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+			"I bore my house upon my back!\n");
+}
+
