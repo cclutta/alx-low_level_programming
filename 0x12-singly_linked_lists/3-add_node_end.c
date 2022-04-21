8d97969b398b57cf0d3eb115907642c95a1e68fe
cclutta <52520502+cclutta@users.noreply.github.com>
2022-04-21 17:01:42 +0300

    Create 3-add_node_end.c

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,40 @@
+#include "lists.h"
+
+/**
+* add_node_end - that adds a new node
+* @head: list_t
+* @str: string
+*
+* Return: list_t
+*/
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new = malloc(sizeof(list_t));
+	list_t *last = *head;
+	unsigned int len = 0;
+
+	if (!new)
+		return (NULL);
+
+	while (str[len])
+		len++;
+
+
+	new->str = strdup(str);
+	new->len = len;
+	new->next = NULL;
+	
+	if (!*head)
+	{
+		*head = new;
+		return;
+	}
+	
+	while (last->next != NULL)
+		last = last->next;
+	
+	last->next = new;
+
+	return (new);
+
+}
