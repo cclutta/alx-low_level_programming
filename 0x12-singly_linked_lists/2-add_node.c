a9a0a0da8487c8ed30e254dbb40846dd60f7ef7b
cclutta <52520502+cclutta@users.noreply.github.com>
2022-04-21 14:08:31 +0300

    Create 2-add_node.c

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+
+/**
+* add_node - that adds a new node
+* @head: list_t
+* @str: string
+*
+* Return: list_t
+*/
+list_t *add_node(list_t **head, const char *str)
+{
+  list_t new = malloc(sizeof(list_t);
+
+}
