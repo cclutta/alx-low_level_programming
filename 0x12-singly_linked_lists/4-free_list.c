8432b0fc350faf4c140e2f800fc49f73825832d1
cclutta <52520502+cclutta@users.noreply.github.com>
2022-04-21 17:12:54 +0300

    Create 4-free_list.c

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+* free_list - that frees a list_t list
+* @head: list_t
+*
+* Return: void
+*/
+void free_list(list_t *head)
+{
+	list_t *t;
+	
+	while (head)
+	{
+		t = head;
+		head = head->next;
+		free(t);
+	}
+}
