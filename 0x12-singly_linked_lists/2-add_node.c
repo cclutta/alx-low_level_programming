a33e8dfc5630446bccd2e5fa2fe2c4b40e07eb22
cclutta <52520502+cclutta@users.noreply.github.com>
2022-04-21 16:55:49 +0300

    Update 2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -11,13 +11,13 @@ list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new = malloc(sizeof(list_t));
 	unsigned int len = 0;
-	
+
 	if (!new)
 		return (NULL);
-	
+
 	while (str[len])
 		len++;
-	
+
 
 	new->str = strdup(str);
 	new->len = len;
