c5623d4a3511074e0fc8c9708aaf845f5c9d99a6
cclutta <52520502+cclutta@users.noreply.github.com>
2022-04-25 23:52:34 +0300

    Update 3-add_nodeint_end.c

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -7,7 +7,7 @@
 *
 * Return: listint_t
 */
-listint_t *add_nodeint_end(list_t **head, const int n)
+listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 	listint_t *new = malloc(sizeof(listint_t));
 	listint_t *last;
