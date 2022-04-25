3f229293619ea4317115e9bab9155ce633072807
cclutta <52520502+cclutta@users.noreply.github.com>
2022-04-25 23:51:04 +0300

    Create 3-add_nodeint_end.c

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,36 @@
+include "lists.h"
+
+/**
+* add_nodeint_end - that adds a new node
+* @head: list_t
+* @n: int
+*
+* Return: listint_t
+*/
+listint_t *add_nodeint_end(list_t **head, const int n)
+{
+	listint_t *new = malloc(sizeof(listint_t));
+	listint_t *last;
+	
+	if (!new)
+		return (NULL);
+
+	new->n = n;
+	new->next = NULL;
+
+	if (!*head)
+	{
+		*head = new;
+	}
+	else
+	{
+		last = *head;
+
+		while (last->next != NULL)
+			last = last->next;
+
+		last->next = new;
+	}
+	return (new);
+
+}
