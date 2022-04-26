963b69fef4461ba6dd138e4c2e78ad7eed35e082
cclutta <52520502+cclutta@users.noreply.github.com>
2022-04-27 00:47:31 +0300

    Update 9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -18,9 +18,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 				 return (NULL);
 	
 	if (!(*head) && idx ! = 0)
-	{
-		return (NULL);
-	}
+				 return (NULL);
+	
 	if (idx != 0)
 	{
 		t = *head;
