3202fd5dfdf9b0e30081acf1590fe0dac8115f5a
cclutta <52520502+cclutta@users.noreply.github.com>
2022-04-26 18:12:31 +0300

    Create 7-get_nodeint.c

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+
+/**
+* get_nodeint_at_index - get node at index
+* @head: listint_t
+*
+* Return: int
+*/
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	listint_t *t, *node;
+	int len;
+	
+	if (head == NULL)
+		return (NULL);
+	
+	while (head)
+	{
+		t = head->next;
+		len++;
+		
+		if (len == index)
+		{
+			node = t;
+			
+			if (node == NULL)
+				return (NULL);
+		}
+		
+	}
+		
+		return (node);
+	
+}
