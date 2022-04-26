15ad00c3638222664a29cb9c85adb7c9c2a30244
cclutta <52520502+cclutta@users.noreply.github.com>
2022-04-26 22:50:09 +0300

    Create 8-sum_listint.c

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,20 @@
+include "lists.h"
+
+/**
+* sum_listint - get node at index
+* @head: listint_t
+*
+* Return: int
+*/
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+	
+	while (head)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+
+	return (sum);
+}
