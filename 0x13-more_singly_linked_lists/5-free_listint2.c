7c1ad5bde66e16d1420c1582cea0de1d0f706b45
cclutta <52520502+cclutta@users.noreply.github.com>
2022-04-26 17:45:48 +0300

    Create 5-free_listint2.c

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+* free_listint2 - that frees a listint_t list
+* @head: listint_t
+*
+* Return: void
+*/
+void free_listint2(listint_t *head)
+{
+	listint_t *t;
+
+	while (head)
+	{
+		t = head->next;
+		free(head);
+		head = t;
+	}
+  
+  head = NULL;
+}
