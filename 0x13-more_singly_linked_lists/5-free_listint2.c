7d9bc9bd02b574561bb253e986546871585e55ad
cclutta <52520502+cclutta@users.noreply.github.com>
2022-04-26 17:51:07 +0300

    Update 5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,13 +9,16 @@
 void free_listint2(listint_t **head)
 {
 	listint_t *t;
+	
+	if (head == NULL)
+		return;
 
 	while (*head)
 	{
-		t = *head->next;
+		t = (*head)->next;
 		free(*head);
 		*head = t;
 	}
   
-  *head = NULL;
+  
 }
