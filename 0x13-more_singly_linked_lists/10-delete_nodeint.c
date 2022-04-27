0c30b606a6711314f37ec663fd4e8da8b0536f22
cclutta <52520502+cclutta@users.noreply.github.com>
2022-04-27 22:23:03 +0300

    Update 10-delete_nodeint.c

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -11,7 +11,7 @@
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 	listint_t *current, *next;
-	int res;
+	unsigned int res;
 	
 	if (head == NULL || *head == NULL)
 		return (-1);
