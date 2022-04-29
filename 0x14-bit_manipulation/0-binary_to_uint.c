c279aa71daecb07db6b06b4613e4eef822680099
cclutta <52520502+cclutta@users.noreply.github.com>
2022-04-29 15:41:58 +0300

    Update 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,1 +1,29 @@
 #include "main.h"
+
+/**
+* binary_to_uint - converts binary to int
+* @b: char of 0s and 1s
+*
+* Return: uint
+*/
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int res;
+	int i = 0;
+	
+	if (!b)
+		return (0);
+	
+	while (b[i] != '\0')
+	{
+		if (b[i] < '0' || b[i] > '1')
+			return (0);
+		val = 2 * val + (b[i] - '0'); 
+		
+		i++;
+	}
+		
+	return (val);
+	
+}
