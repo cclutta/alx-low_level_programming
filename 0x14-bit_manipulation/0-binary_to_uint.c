7d632fb72a265457a7ecbe54df44a001c2cd8c95
cclutta <52520502+cclutta@users.noreply.github.com>
2022-04-29 15:45:29 +0300

    Update 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -11,19 +11,17 @@ unsigned int binary_to_uint(const char *b)
 {
 	unsigned int res;
 	int i = 0;
-	
+
 	if (!b)
 		return (0);
-	
+
 	while (b[i] != '\0')
 	{
 		if (b[i] < '0' || b[i] > '1')
 			return (0);
-		res = 2 * res + (b[i] - '0'); 
-		
+		res = 2 * res + (b[i] - '0');
 		i++;
 	}
-		
 	return (res);
-	
+
 }
