526764b79780be1d947da9a75f9103172262f4ee
cclutta <52520502+cclutta@users.noreply.github.com>
2022-05-03 22:46:35 +0300

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,9 +1,48 @@
 #include "main.h"
 
 /**
+* read_textfile - reads a text file and prints to stdout
+* @filename: file
+* @letters: to print
 *
+* Return: ssize_t
 */
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
+	char *buffer;
+	int f;
+	int i, res;
+	
+	if (!filename)
+		return (0);
+	
+	f = open(filename, O_RDONLY);
+	
+	if (f < 0)
+		return (0);
+	
+	buffer = malloc(sizeof(char) * letters);
+	
+	if (!buffer)
+		return (0);
+	
+	i = read(f, buffer, letters);
+	if (i < 0)
+	{
+		free(buffer);
+		return (0);
+	}
+	
+	buffer[i] = '\0';
+	close(f);
+	res = write(STDOUT_FILENO, buffer, i);
+	if (res < 0)
+	{
+		free(buffer);
+		return (0);
+	}
+	free(buffer);
+	return (res);
+	
 }
