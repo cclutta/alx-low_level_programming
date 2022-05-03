25ec8fd314cc071535907632f2ea9b34eae9f9ad
cclutta <52520502+cclutta@users.noreply.github.com>
2022-05-03 23:04:53 +0300

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,10 +12,10 @@ int create_file(const char *filename, char *text_content)
 {
 	int f, x;
 	int y = 0;
-	
+
 	if (!filename)
 		return (-1);
-	
+
 	f = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (f < 0)
 		return (-1);
