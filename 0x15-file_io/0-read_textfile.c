d2e7d41e21e106695e95e09779644d66a7ac176b
cclutta <52520502+cclutta@users.noreply.github.com>
2022-05-03 22:49:53 +0300

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -13,27 +13,27 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	char *buffer;
 	int f;
 	int i, res;
-	
+
 	if (!filename)
 		return (0);
-	
+
 	f = open(filename, O_RDONLY);
-	
+
 	if (f < 0)
 		return (0);
-	
+
 	buffer = malloc(sizeof(char) * letters);
-	
+
 	if (!buffer)
 		return (0);
-	
+
 	i = read(f, buffer, letters);
 	if (i < 0)
 	{
 		free(buffer);
 		return (0);
 	}
-	
+
 	buffer[i] = '\0';
 	close(f);
 	res = write(STDOUT_FILENO, buffer, i);
@@ -44,5 +44,5 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	}
 	free(buffer);
 	return (res);
-	
+
 }
