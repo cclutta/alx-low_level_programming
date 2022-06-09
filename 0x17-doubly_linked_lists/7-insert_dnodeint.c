0bea10383e7c4d2964886693e21bdaaedaeee419
cclutta <52520502+cclutta@users.noreply.github.com>
2022-06-10 00:01:52 +0300

    Create 7-insert_dnodeint.c

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,63 @@
+#include "lists.h"
+
+/**
+* dlistint_len -  returns the number of elements
+* @h: head
+*
+* Return: size_t
+*/
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		i++;
+		h = h->next;
+	}
+	return (i);
+}
+
+/**
+* insert_dnodeint_at_index - add new node at index
+* @h: node
+* @idx: index
+* @n: int
+*
+* Return: new node
+*/
+
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
+{
+	dlistint_t *new = NULL;
+	dlistint_t *current = *h;
+	size_t len = dlistint_len(*h);
+	
+	if (h)
+	{
+		if (idx == 0)
+		{
+			new = add_dnodeint(h, n);
+			return (new);
+		}
+		if (idx == len)
+		{
+			new = add_dnodeint_end(h, n);
+			return (new);
+		}
+		new = malloc(sizeof(dlistint));
+		if (new)
+		{
+			new->n = n;
+			while (idx--)
+			{
+				current = current->next;
+			}
+			new->prev = current->prev;
+			new->next = current;
+			current->prev->next = new;
+			current->prev = new;
+		}
+	}
+	return (new);
+}
