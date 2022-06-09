08a3d30b27f5666f5067b0f3583c77739d363e17
cclutta <52520502+cclutta@users.noreply.github.com>
2022-06-09 15:07:23 +0300

    Update 0-print_dlistint.c

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -2,19 +2,19 @@
 
 /**
 * print_dlistint - that prints all the elements of a dlistint_t
-* @h - head 
+* @h: head
 *
 * Return: size_t
 */
 size_t print_dlistint(const dlistint_t *h)
 {
 	size_t i = 0;
-	
+
 	while (h)
 	{
 		printf("%d\n", h->n);
 		i++;
 		h = h->next;
 	}
-	return i;
+	return (i);
 }
