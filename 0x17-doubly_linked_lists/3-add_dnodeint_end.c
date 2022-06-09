c87db99032c71cad62b4fbc3d0efef9c31b16d88
cclutta <52520502+cclutta@users.noreply.github.com>
2022-06-09 21:53:10 +0300

    Create 3-add_dnodeint_end.c

---
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+
+/**
+* add_dnodeint_end - add new node at the beginning
+* @head: node
+* @n: int
+*
+* Return: new node
+*/
+
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *new = NULL
+    dlsitint_t *current = *head;
+
+	new = malloc(sizeof(dlistint_t));
+
+	if (new)
+	{
+		new->n = n;
+		new->next = NULL;
+        new->prev = NULL;
+
+		if (!(*head))
+		{
+			*head = new;
+		}
+        else
+        {
+            while (current->next)
+                current = current->next;
+            current->next = new;
+            new->prev = current;
+        }
+	}
+	return (new);
+}
