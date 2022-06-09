6b3633861e9a4121871f657e19bd90e558612c63
cclutta <52520502+cclutta@users.noreply.github.com>
2022-06-09 15:08:45 +0300

    Update 1-dlistint_len.c

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,1 +1,19 @@
+#include "lists.h"
 
+/**
+* dlistint_len -  returns the number of elements
+* @h: head
+*
+* Return: size_t
+*/
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		i++;
+		h = h->next;
+	}
+	return (i);
+}
