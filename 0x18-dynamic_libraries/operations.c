cf9f6ce23e022b53de5d37020af1b3e43e02e975
cclutta <52520502+cclutta@users.noreply.github.com>
2022-06-15 12:32:30 +0300

    Create operations.c

---
+++ 0x18-dynamic_libraries/operations.c
@@ -0,0 +1,62 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+* add - add numbers
+* @a: first
+* @b: second
+*
+* Return: int
+*/
+int add(a, b)
+{
+	return (a + b);
+}
+
+/**
+* sub - sub numbers
+* @a: first
+* @b: second
+*
+* Return: int
+*/
+int sub(a, b)
+{
+	return (a - b);
+}
+
+/**
+* mul - multiply numbers
+* @a: first
+* @b: second
+*
+* Return: int
+*/
+int mul(a, b)
+{
+	return (a * b);
+}
+
+/**
+* div - divide numbers
+* @a: first
+* @b: second
+*
+* Return: int
+*/
+int div(a, b)
+{
+	return (a / b);
+}
+
+/**
+* mod - mod numbers
+* @a: first
+* @b: second
+*
+* Return: int
+*/
+int mod(a, b)
+{
+	return (a % b);
+}
