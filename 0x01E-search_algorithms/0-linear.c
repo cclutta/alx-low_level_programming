a6679e6227a2e752a9e564b042010abac04ffe20
cclutta <52520502+cclutta@users.noreply.github.com>
2022-10-03 17:08:22 +0300

    Update 0-linear.c

--- 0x01E-search_algorithms/0-linear.c
+++ 0x01E-search_algorithms/0-linear.c
@@ -11,7 +11,8 @@
 
 int linear_search(int *array, size_t size, int value)
 {
-    size_t i; 
+    size_t i;
+
     if (!array)
         return (-1);
     for (i = 0; i < size; i++)
@@ -19,8 +20,8 @@ int linear_search(int *array, size_t size, int value)
         printf("Value checked array[%lu] = [%d]\n", i, array[i]);
         if (array[i] == value)
         {
-            return (i); 
-        }
-    }
-    return (-1);
+            return (i);
+         }
+     }
+     return (-1);
 }
