df35712c5bd2876718002e10e74e051dd5c0051d
cclutta <52520502+cclutta@users.noreply.github.com>
2022-10-03 16:47:55 +0300

    Create search_algos.h

---
+++ 0x01E-search_algorithms/search_algos.h
@@ -0,0 +1,10 @@
+#ifndef FILE_SEARCH_ALGOS
+#define FILE_SEARCH_ALGOS
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int linear_search(int *array, size_t size, int value);
+
+#endif
